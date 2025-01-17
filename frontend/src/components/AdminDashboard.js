74d5a8f1ce7387b0cdee144db84846c0150d098d
Amol Rathod <amolr2107@gmail.com>
2025-01-17 19:20:52 +0530

    Description of the changes

--- frontend/src/components/AdminDashboard.js
+++ frontend/src/components/AdminDashboard.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react'
+import Spinner from './Spinner';
 
 function AdminDashboard() {
   const [users, setUsers] = useState([]);
-
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
+    setLoading(true);
     async function fetchUsers() {
       const response = await fetch("https://social-media-application-task-backend.onrender.com/api/v1/allData");
       const data = await response.json();
@@ -11,10 +13,16 @@ function AdminDashboard() {
     }
 
     fetchUsers();
+    setLoading(false);
   });
 
   return (
     <div className="p-4">
+      {loading && (
+        <div className="absolute top-0 left-0 w-full h-full bg-white bg-opacity-80 flex items-center justify-center z-10">
+          <Spinner />
+        </div>
+      )}
       <h1 className="text-3xl font-semibold mb-6">Admin Dashboard</h1>
       { users.length > 0 ? (
         users.map((user) => (
